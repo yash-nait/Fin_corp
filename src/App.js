9cacf9884d54d9de4d5bef3f6e0c79cfbfaeb358
yash-nait <yashnait@gmail.com>
2021-10-09 17:37:59 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App bkg">
+      <div>
+        <div class="row">
+          <div class="col-8">
+            Column
+          </div>
+          <div class="col-4">
+            Sukant and bhatia
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
